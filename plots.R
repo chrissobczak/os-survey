a3f6b20eaa540920b662b53f383be7cfa33bb70a
Chris Sobczak <csobczak@sfu.ca>
2021-04-18 18:29:45 -0700

    Final touches to report

--- plots.R
+++ plots.R
@@ -2,6 +2,7 @@ library(dplyr)
 load('audit/weights.RData')
 
 missing_props <- data.frame(country=weights[-5,1],missingprop=(weights[-5,2]-weights[-5,3])/weights[-5,2])
+orig_missing_countries=missing_props$country
 missing_props$country <- gsub(',\\sRepublic.*$','',missing_props$country)
 missing_props$country <- gsub('Syrian.*$','Syria',missing_props$country)
 missing_props$country <- gsub(',\\sBoliva.*','',missing_props$country)
@@ -31,8 +32,15 @@ c_props <- data.frame(country=c_s,prop=df[,'FOSS'])
 c_props <- c_props %>%
 	group_by(country) %>%
 	summarise(mean=mean(prop,na.rm=T))
-c_props <- c_props[which(c_props$country %in% missing_props$country),]
+c_props <- c_props[which(c_props$country %in% orig_missing_countries),]
 c_props <- c_props[order(c_props[,2],decreasing=T),]
+o_props <- left_join(missing_props,c_props,by='country')
+
+c_props$country <- gsub(',\\sRepublic.*$','',c_props$country)
+c_props$country <- gsub('Syrian.*$','Syria',c_props$country)
+c_props$country <- gsub(',\\sBoliva.*','',c_props$country)
+c_props$country <- gsub('n\\sFed.*','',c_props$country)
+
 pdf('report/graphics/country_props.pdf')
 par(mar=c(7.1,4.1,4.1,2.1))
 barplot(
